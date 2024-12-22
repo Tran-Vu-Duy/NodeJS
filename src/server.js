ba1a5bd99c20f5c1a2bff14c68060d7db7c0db8a
Tran-Vu-Duy <tranvud528@gmail.com>
2024-12-22 16:28:35 +0700

    DB

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewengine";
 import webRoutes from "./routes/web";
+import connectDB from "./config/connectDB";
 
 require("dotenv").config();
 let app = express();
 viewEngine(app);
+webRoutes(app);
+connectDB();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-webRoutes(app);
+
 
 let port = process.env.PORT || 6969;
 
